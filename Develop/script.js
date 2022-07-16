47cc470f0f2074eaf7dc6a5fe29c652a7730a645
aurumbright <aurum.elean@gmail.com>
2022-07-16 13:32:07 -0700

    replaced dayjs with moment

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,28 @@
 let dateDisplayEl = $('#currentDay');
 
 let timeSlot = $('.time-slot').text();
+let time = moment(timeSlot, 'h a').format('h a');
 
-// this is wrong I think 
-let time = dayjs(timeSlot, 'h a').format('h a');
-
-
+console.log(time)
 
 // Clock for top of app
-let currentDay = dayjs().format("dddd MMM D YYYY hh:mm:ss a");
+let currentDay = moment().format("dddd MMM Do YYYY hh:mm:ss a");
 dateDisplayEl.text(currentDay);
 
 function updateDay() {
-    currentDay = dayjs().format("dddd MMM D YYYY hh:mm:ss a");
+    currentDay = moment().format("dddd MMM Do YYYY hh:mm:ss a");
     dateDisplayEl.text(currentDay);
 };
 setInterval(updateDay, 1000);
 
 // this sort of works but needs to be applied to each row - maybe a forEach?
-// still not working...
 
 $('.row').each(function(index) {
 
-    if (dayjs().isBefore(dayjs(time, 'hour'))) {
-        $('input').addClass("future");
-    } else if (dayjs().isAfter(dayjs(time, 'hour'))) {
+    if (moment(time, 'h a').isBefore(moment())) {
         $('input').addClass("past");
+    } else if (moment(time).isAfter(moment())) {
+        $('input').addClass("future");
     } else {
         $('input').addClass("present");
     }
@@ -33,11 +30,10 @@ $('.row').each(function(index) {
 
 
 // item one: savebtn saves content of row to localStorage to be reconstituted on reload
-// this will probably be setting it as placeholder text? or something like that
 
 // item two: past / present / future styling applied to rows or maybe just input based on time
 /* 
-If textContent of time-slot when converted to dayjs() format is less than current dayjs(), 
+If textContent of time-slot when converted to moment() format is less than current moment(), 
 apply .past styling
 
 
