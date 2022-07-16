8d877eff957a6592447f30796f5318715cb7b2d6
aurumbright <aurum.elean@gmail.com>
2022-07-16 13:30:05 -0700

    built out webpage adding js

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,44 @@
-let timeDisplayEl = $('#currentDay');
+let dateDisplayEl = $('#currentDay');
+
+let timeSlot = $('.time-slot').text();
+
+// this is wrong I think 
+let time = dayjs(timeSlot, 'h a').format('h a');
+
 
 
 // Clock for top of app
-let currentTime = moment().format("dddd MMM Do YYYY hh:mm:ss a");
-timeDisplayEl.text(currentTime);
+let currentDay = dayjs().format("dddd MMM D YYYY hh:mm:ss a");
+dateDisplayEl.text(currentDay);
 
-function updateTime() {
-    currentTime = moment().format("dddd MMM Do YYYY hh:mm:ss a");
-    timeDisplayEl.text(currentTime);
+function updateDay() {
+    currentDay = dayjs().format("dddd MMM D YYYY hh:mm:ss a");
+    dateDisplayEl.text(currentDay);
 };
-setInterval(updateTime, 1000);
+setInterval(updateDay, 1000);
 
-//
+// this sort of works but needs to be applied to each row - maybe a forEach?
+// still not working...
+
+$('.row').each(function(index) {
+
+    if (dayjs().isBefore(dayjs(time, 'hour'))) {
+        $('input').addClass("future");
+    } else if (dayjs().isAfter(dayjs(time, 'hour'))) {
+        $('input').addClass("past");
+    } else {
+        $('input').addClass("present");
+    }
+});
+
+
+// item one: savebtn saves content of row to localStorage to be reconstituted on reload
+// this will probably be setting it as placeholder text? or something like that
+
+// item two: past / present / future styling applied to rows or maybe just input based on time
+/* 
+If textContent of time-slot when converted to dayjs() format is less than current dayjs(), 
+apply .past styling
+
+
+*/
