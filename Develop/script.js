2d9e4f225b324c5a49f6864b7ced5f9e6a6178e7
aurumbright <aurum.elean@gmail.com>
2022-07-16 14:40:17 -0700

    working on the .each loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,5 @@
 let dateDisplayEl = $('#currentDay');
 
-let timeSlot = $('.time-slot').text();
-let time = moment(timeSlot, 'h a').format('h a');
-
-console.log(time)
-
 // Clock for top of app
 let currentDay = moment().format("dddd MMM Do YYYY hh:mm:ss a");
 dateDisplayEl.text(currentDay);
@@ -15,26 +10,40 @@ function updateDay() {
 };
 setInterval(updateDay, 1000);
 
-// this sort of works but needs to be applied to each row - maybe a forEach?
-
-$('.row').each(function(index) {
-
-    if (moment(time, 'h a').isBefore(moment())) {
-        $('input').addClass("past");
-    } else if (moment(time).isAfter(moment())) {
-        $('input').addClass("future");
-    } else {
-        $('input').addClass("present");
-    }
-});
-
-
 // item one: savebtn saves content of row to localStorage to be reconstituted on reload
+
+// localStorage.setItem("entry", JSON.stringify(entry));
+
 
 // item two: past / present / future styling applied to rows or maybe just input based on time
 /* 
 If textContent of time-slot when converted to moment() format is less than current moment(), 
 apply .past styling
-
-
 */
+
+// make sure to only addClass to input child of current row element
+
+// may need to add array for rows to loop through as ids
+
+
+$('.row').each(function() {
+
+    let timeSlot = $('.time-slot').text();
+    let time = moment(timeSlot, 'h a');
+
+    if (moment(time, 'h a').isBefore(moment())) {
+        $(this).children('input').addClass("past");
+        $(this).children('input').removeClass("present");
+        $(this).children('input').removeClass("future");
+
+    } else if (moment(time, 'h a').isAfter(moment())) {
+        $(this).children('input').addClass("future");
+        $(this).children('input').removeClass("present");
+        $(this).children('input').removeClass("past");
+
+    } else {
+        $(this).children('input').addClass("present");
+        $(this).children('input').removeClass("past");
+        $(this).children('input').removeClass("future");
+    }
+});
